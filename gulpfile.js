165ded8be3933f47b463e6a3139349d0aaf2fd01
reaganthomas <reagan_thomas@baylor.edu>
2015-08-29 21:13:12 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+(function() {
+  var gulp = require('gulp');
+  var mocha = require('gulp-mocha');
+
+  gulp.task('test', function() {
+    return gulp.src('test/**/*.js', { read: false })
+      .pipe(mocha());
+  });
+})();
